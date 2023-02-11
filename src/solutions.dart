bbcfc24103913ae5b24bcd1f1a9be2271b053c0b
PavPavv <pavpavdev@gmail.com>
2023-02-12 00:17:01 +0400

    feat: fix styling, add three more solutions

--- src/solutions.dart
+++ src/solutions.dart
@@ -1,5 +1,5 @@
 void main() {
-  print('hello, world!');
+  // print('hello, world!');
   //  1
   int findFirstLeft(List<int> arr, int target) {
     for (int i = 0; i < arr.length; i++) {
@@ -10,7 +10,7 @@ void main() {
     return -1;
   }
 
-  print(findFirstLeft([1, 2, 3, 4, 5, 6, 7], 3)); //  3
+  // print(findFirstLeft([1, 2, 3, 4, 5, 6, 7], 3)); //  3
 
   //  2
   int findFirstRight(List<int> arr, int target) {
@@ -22,13 +22,51 @@ void main() {
     return -1;
   }
 
-  print(findFirstRight([1, 2, 3, 4, 5, 3, 6, 7], 3)); //  5
+  // print(findFirstRight([1, 2, 3, 4, 5, 3, 6, 7], 3)); //  5
 
   // 3
   bool isPalindrome(String s) {
     return s.toLowerCase() == s.toLowerCase().split('').reversed.join('');
   }
 
-  print(isPalindrome('Ana')); //  true
-  print(isPalindrome('Mark')); //  false
+  // print(isPalindrome('Ana')); //  true
+  // print(isPalindrome('Mark')); //  false
+
+  // 4
+  String reverseStr(String s) {
+    String result = '';
+    int i = 0;
+
+    while (i < s.length) {
+      i++;
+      result += s[s.length - i];
+    }
+    return result;
+  }
+  // print(reverseStr('test'));
+
+  // 5
+  Iterable<String> findPersonWithCertainAmountOfLetters(
+      List<String> names, int amount) {
+    return names.where((name) => name.length == amount);
+  }
+
+  // print(
+  //     findPersonWithCertainAmountOfLetters(['John', 'Jack', 'Dean', 'Pat'], 3));
+  // print(findPersonWithCertainAmountOfLetters(
+  //     ['John', 'Mag', 'Jack', 'Dean', 'Pat'], 3));
+
+  // 6
+  int countVowels(String str) {
+    List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+    int counter = 0;
+    for (int i = 0; i < str.length; i++) {
+      if (vowels.contains(str[i])) {
+        counter++;
+      }
+    }
+    return counter;
+  }
+
+  print(countVowels('some')); //  2
 }
